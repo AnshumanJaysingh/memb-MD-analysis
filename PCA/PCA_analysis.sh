4c79117dd02e5702d15ad0ec76744e9b3c6a4e58
AnshumanJaysingh <anshumanjaysingh41478@gmail.com>
2022-02-01 15:16:18 +0530

    PCA automation added

--- PCA/PCA_analysis.sh
+++ PCA/PCA_analysis.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
+
+#Put the opology (gro) file, trajectory (xtc) files and this script in the same folder 
+
+TOP=$input_file.gro #set topologu file name
+setFramesAnim=30
+
 mkdir -p PCA
-for FILE in *.xtc
+for TRAJ in *.xtc
 do
     
-    NAME=`echo "$FILE" | cut -d'.' -f1` #separate file name and extension
+    NAME=`echo "$TRAJ" | cut -d'.' -f1` #separate TRAJ name and extension
     mkdir -p PCA/$NAME
 
     tput setaf 3; tput bold
     echo "INPUT TRAJECTORY:   " $NAME
     tput sgr0
+
+    # Note that the calculation would be very slow if an atom group containing a lot of atoms are selected 
+
       
-    printf '4\n4\n' | gmx covar -s input_file.gro -f $FILE
+    printf '4\n4\n' | gmx covar -s $TOP -f $TRAJ
     mv eigenvec.trr eigenval.xvg average.pdb covar.log PCA/$NAME
-    #cp covar.sh $FILE input_file.gro $NAME
 
     cd PCA/$NAME
     
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc1.pdb -first 1 -last 1 -nframes 30
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc2.pdb -first 2 -last 2 -nframes 30
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc3.pdb -first 3 -last 3 -nframes 30  
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc4.pdb -first 4 -last 4 -nframes 30    
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc5.pdb -first 5 -last 5 -nframes 30
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc6.pdb -first 6 -last 6 -nframes 30
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc7.pdb -first 7 -last 7 -nframes 30
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc8.pdb -first 8 -last 8 -nframes 30
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc9.pdb -first 9 -last 9 -nframes 30
-    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../input_file.gro -f ../../$FILE -eig eigenval.xvg -extr pc10.pdb -first 10 -last 10 -nframes 30
+    # select 4th index group for backbone. Change as necessary
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc1.pdb -first 1 -last 1 -nframes $setFramesAnim
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc2.pdb -first 2 -last 2 -nframes $setFramesAnim
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc3.pdb -first 3 -last 3 -nframes $setFramesAnim  
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc4.pdb -first 4 -last 4 -nframes $setFramesAnim    
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc5.pdb -first 5 -last 5 -nframes $setFramesAnim
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc6.pdb -first 6 -last 6 -nframes $setFramesAnim
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc7.pdb -first 7 -last 7 -nframes $setFramesAnim
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc8.pdb -first 8 -last 8 -nframes $setFramesAnim
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc9.pdb -first 9 -last 9 -nframes $setFramesAnim
+    printf '4\n4\n' | gmx anaeig -v eigenvec.trr -s ../../$TOP -f ../../$TRAJ -eig eigenval.xvg -extr pc10.pdb -first 10 -last 10 -nframes $setFramesAnim
     
     cd ../../
     
